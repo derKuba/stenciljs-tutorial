ab617abe39f731622318379994472dc71d2f1b81
Kuba <pawlik.jacob@gmail.com>
2021-10-17 13:31:20 +0200

    add storysource plugin

--- component-lib/.storybook/main.js
+++ component-lib/.storybook/main.js
@@ -1,8 +1,26 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     // 'storybook-custom-event-broadcaster/dist/presets.js'
+    // {
+    //   name: '@storybook/addon-storysource',
+    //   options: {
+    //     rule: {
+    //       // test: [/\.stories\.jsx?$/], This is default
+    //       include: [path.resolve(__dirname, '../src')], // You can specify directories
+    //     },
+    //     loaderOptions: {
+    //       prettierConfig: { printWidth: 80, singleQuote: false },
+    //     },
+    //     sourceLoaderOptions: {
+    //       injectStoryParameters: false,
+    //     },
+    //   },
+    // },
+    '@storybook/addon-storysource',
   ],
 };
