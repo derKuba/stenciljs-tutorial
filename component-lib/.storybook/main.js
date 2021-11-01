e1afbc2c175d3ecb73d538f472778e89064f4d3a
Kuba <pawlik.jacob@gmail.com>
2021-11-01 13:37:00 +0100

    fix example project

--- component-lib/.storybook/main.js
+++ component-lib/.storybook/main.js
@@ -2,15 +2,5 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: 'storybook-custom-event-broadcaster/dist/presets.js',
-      options: {
-        useGlobal: true,
-        bla: 'Blub',
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-custom-event-broadcaster/dist/register'],
 };
--- component-lib/stencil.config.ts
+++ component-lib/stencil.config.ts
@@ -3,22 +3,22 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'component-lib',
   outputTargets: [
-    // {
-    //   type: 'dist',
-    //   esmLoaderPath: '../loader',
-    // },
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
     {
       type: 'dist-custom-elements',
     },
-    // {
-    //   type: 'dist-custom-elements-bundle',
-    // },
-    // {
-    //   type: 'docs-readme',
-    // },
-    // {
-    //   type: 'www',
-    //   serviceWorker: null, // disable service workers
-    // },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
   ],
 };
