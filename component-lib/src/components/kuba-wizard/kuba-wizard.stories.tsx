63c9b92a3e5e701f255ce916bd58734c8ff57b10
Kuba <pawlik.jacob@gmail.com>
2021-11-07 20:20:22 +0100

    update package.json

--- component-lib/src/components/kuba-wizard/kuba-wizard.stories.tsx
+++ component-lib/src/components/kuba-wizard/kuba-wizard.stories.tsx
@@ -1,31 +1,20 @@
 export default {
-  title: "Content/Wizards",
+  title: 'Content/Wizards',
   parameters: {
     customEventPanel: [
       {
-
-        eventName: "wizard:next",
-        eventData: {
-          opt: 23
-        },
-        selector: "mein.selektor"
+        eventName: 'wizard:next',
       },
       {
-
-        eventName: "wizard:previous",
+        eventName: 'wizard:previous',
         eventData: {
-          bla: 23
-        }
+          bla: 23,
+        },
       },
-
     ],
-
   },
-  argTypes: {
-
-  }
+  argTypes: {},
 };
 
-const Template = () => `<kuba-wizard></kuba-wizard>`
+const Template = () => `<kuba-wizard></kuba-wizard>`;
 export const Wizard = Template.bind({});
-
