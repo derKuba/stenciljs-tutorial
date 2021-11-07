c734aaef85afd3eb61dab6d8063af0d0544f7821
Jacob Pawlik <jacob.pawlik@devk.de>
2021-11-07 12:41:53 +0100

    fix addon loading

--- component-lib/.storybook/main.js
+++ component-lib/.storybook/main.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-custom-event-broadcaster/dist/register'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-custom-event-broadcaster/dist'],
 };
